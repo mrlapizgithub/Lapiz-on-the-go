e018eb439b01852b1d59b6d8128b4acb39a8f77f
Junhao Zhang <thepuzzlecoderuser@gmail.com>
2019-11-21 03:43:37 +0000

    I made it better

--- public/script.js
+++ public/script.js
@@ -1,13 +1,39 @@
 var apps = [function(body, game, random){
-  body.html(
-    '<h1 class = "font1 text-center">Spanish numbers</h1><p class = "ml-2">How to play: Translate the text that is given to you. If you get it correct, your score increases, if you get it wrong, your score resets to 0. Have fun!</p><div class = "m-2 p-2 bg-primary rounded shadow-cus1"><p class = "m-0">Your score: <span class = "score">0</span></p><p class = "mb-1">Turn <span class = "q-cont">Loading...</span> into a number.</p><div class = "input-group mb-3"><input type = "number" class = "form-control answer" placeholder = "Your answer" min = "0" max = "10"><div class="input-group-append"><button class = "btn btn-success submit" type = "button" id = "button-addon2">Submit</button></div></div></div>'
-  );
+  body.html('<h1 class = "font1 text-center">Spanish numbers</h1><p class = "ml-2">How to play: Translate the text that is given to you. If you get it correct, your score increases, if you get it wrong, your score resets to 0. Have fun!</p><div class = "m-2 p-2 bg-primary rounded shadow-cus1"><p class = "m-0">Your score: <span class = "score">0</span></p><p class = "mb-1">Turn <span class = "q-cont">Loading...</span> into a number.</p><div class = "input-group mb-3"><input type = "number" class = "form-control answer" placeholder = "Your answer" min = "0" max = "10"><div class="input-group-append"><button class = "btn btn-success submit" type = "button" id = "button-addon2">Submit</button></div></div></div>');
   var q = [{q:"cero",a:0},{q:"uno",a:1},{q:"dos",a:2},{q:"tres",a:3},{q:"cuatro",a:4},{q:"cinco",a:5},{q:"seis",a:6},{q:"siete",a:7},{q:"ocho",a:8},{q:"nueve",a:9},{q:"diez",a:10}];
   initgame($('.q-cont'),q,$('.answer'),$('.submit'),$('.score'));
 },function(body, game, random){
-  body.html("domo1");
+  body.html('<h1 class = "font1 text-center">Simple Words</h1><p class = "ml-2">How to play: Translate the text that is given to you. If you get it correct, your score increases, if you get it wrong, your score resets to 0. Have fun!</p><div class = "m-2 p-2 bg-primary rounded shadow-cus1"><p class = "m-0">Your score: <span class = "score">0</span></p><p class = "mb-1">What is <span class = "q-cont">Loading...</span> in english?</p><div class = "input-group mb-3"><input class = "form-control answer"><div class="input-group-append"><button class = "btn btn-success submit" type = "button" id = "button-addon2">Submit</button></div></div></div>');
+  var es = "Adiós,Hola,Buenos días,Buenas tardes,Buenas Noches,Chao,Hasta Luego,Hasta mañana,Señor,Señora,Señorita".split(",");
+  var en = "Goodbye,Hello,Good morning,Good afternoon,Good night,Bye,See you later,See you tomorrow,Mister,Misses,Miss".split(",");
+  var q = [];
+  for(var i = 0; i < es.length; i ++){q.push({q:es[i],a:en[i]});}
+  initgame($('.q-cont'),q,$('.answer'),$('.submit'),$('.score'));
 },function(body, game, random){
-  body.html("domo2");
+  body.html('<h1 class = "font1 text-center">Number Guesser</h1><p class = "ml-2">How to play: Guess the number from 1-(a number 2-10). If you get it right, your score increases, if you get it wrong, your score decreases. Have fun!</p><div class = "m-2 p-2 bg-primary rounded shadow-cus1"><p class = "m-0">Your score: <span class = "score">0</span></p><p class = "mb-1">I am thinking of a number from 1-<span class = "q-num">2</span>. What is the number?</p><div class = "input-group mb-3"><input type = "number" class = "form-control answer" min = "1" max = "20"><div class="input-group-append"><button class = "btn btn-success submit" type = "button" id = "button-addon2">Submit</button></div></div></div>');
+  var range = random(2,10);
+  var num = random(1,range);
+  var score = 0;
+  $('.q-num').html(range);
+  $('.submit').click(function(){
+    if($('.answer').val()==num){
+      score+=range;
+      $('.score').html(score);
+    }else{
+      score--;
+      $('.score').html(score);
+      alert("The number was "+num+"!");
+    }
+    range = random(2,10);
+    num = random(1,range);
+    $('.q-num').html(range);
+    $('.answer').val("");
+  });
+  $('.answer').keypress(function(e){
+    if(e.which==13){
+      $('.submit').trigger("click");
+    }
+  });
 }];
 $('.close').click(function(){
   $('.lmodal').fadeOut(190);
